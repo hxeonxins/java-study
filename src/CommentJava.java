e0953bcc14962ccfeed80996d68272f6aadbf1d8
hxeonxins <hyunjin.dev143@gmail.com>
2023-12-09 01:25:58 +0900

    first commit

---
+++ src/CommentJava.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java를 출력합니다.
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
---
+++ src/variable/Var8.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        //정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; // -32,768 ~ 32767
+        int i = 2147483647; // 약 20억
+
+        //20억보다 훨씬 큼
+        long l = 9223372036854775807L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
