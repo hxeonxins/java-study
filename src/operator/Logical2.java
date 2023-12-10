81864e958bb279f9d522837ea53d3e4cd6575fb0
hxeonxins <hyunjin.dev143@gmail.com>
2023-12-10 15:10:15 +0900

    FEAT : Operator
    연산자 최종 끝

--- src/operator/Logical2.java
+++ src/operator/Logical2.java
@@ -4,6 +4,7 @@ public class Logical2 {
     public static void main(String[] args) {
         int a = 15;
         //a는 10보다 크고 20보다 작다
-        boolean result = 
+        boolean result = 10 < a && a < 20; //(a>10) && (a<20)
+        System.out.println("result = " + result);
     }
 }
---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+        boolean result = score >= 80 && score <= 100;
+        System.out.println(result); //첫시도 틀림..나중에 한번 더 풀어보기!
+    }
+}
