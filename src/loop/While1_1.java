869cd7613e584a0543b84c8ce739d10b0eee207f
hxeonxins <hyunjin.dev143@gmail.com>
2023-12-12 19:40:26 +0900

    FEAT : While
    반복문

---
+++ src/loop/While1_1.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class While1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        count++;
+        System.out.println("현재 값은 " + count);
+        count++;
+        System.out.println("현재 값은 " + count);
+        count++;
+        System.out.println("현재 값은 " + count);
+    }
+}
