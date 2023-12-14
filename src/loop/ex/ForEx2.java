2eb143596e451aa46083a6d289530120fb61ee2a
hxeonxins <hyunjin.dev143@gmail.com>
2023-12-14 21:01:48 +0900

    FEAT : Loop
    반복문

---
+++ src/loop/ex/ForEx2.java
@@ -0,0 +1,11 @@
+package loop.ex;
+
+public class ForEx2 {
+
+    public static void main(String[] args) {
+        for (int num = 2, count = 1; count <= 10; num += 2, count++) {
+            System.out.println(num);
+        }
+        //int num 을 앞에 빼는게 더 깔끔해 보이긴 함
+    }
+}
